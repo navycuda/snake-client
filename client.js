958e01bfa858317b11bd53b82143c8bb3b8e6ea2
Shane <navysupra@gmail.com>
2022-08-03 20:01:29 +0000

    cleanup

--- client.js
+++ client.js
@@ -3,17 +3,17 @@ const { on } = require("events");
 const net = require(`net`);
 
 const connect = function (address, port) {
+  // Create connection to server
   const client = net.createConnection({
     host: address,
     port: port
   });
+  // Set encoding to utf8
   client.setEncoding(`utf8`);
-  
+  // Recieve a message from server
   client.on(`data`, (data) => {
     console.log(`server > `, data);
   });
-  
-  
   return client;
 };
 
--- play.js
+++ play.js
@@ -5,7 +5,5 @@ const connect = require(`./client`);
 const ADDRESS = `165.227.47.243`;
 const PORT = `50541`;
 
-
-
 console.log(`Connecting to ${ADDRESS}:${PORT}...`);
 connect(ADDRESS, PORT);
